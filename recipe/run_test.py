9faeaf8bf4f8960b8a804dca1984ab50130696a2
Ray Donnelly <mingw.android@gmail.com>
2018-06-30 20:28:22 +0000

    Skip tests on Python 3.7, seems like a fairly unmainained project

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,1 +1,7 @@
-from pysnmp.entity.rfc3413.oneliner import cmdgen
+import platform, sys
+
+if platform.python_version_tuple() < ('3', '7', '0'):
+    from pysnmp.entity.rfc3413.oneliner import cmdgen
+else:
+    print("WARNING :: Skipping tests due to asyncio.async() being a syntax error on Python >=3.7.0, pysnmp.asyncio does not work on 3.7 and above"")
+    sys.exit(0)
