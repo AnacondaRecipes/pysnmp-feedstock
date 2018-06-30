be71cdb24aeb271cff9c2157b9c117fe51abf08a
Ray Donnelly <mingw.android@gmail.com>
2018-06-30 20:32:00 +0000

    Oops

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -3,5 +3,5 @@ import platform, sys
 if platform.python_version_tuple() < ('3', '7', '0'):
     from pysnmp.entity.rfc3413.oneliner import cmdgen
 else:
-    print("WARNING :: Skipping tests due to asyncio.async() being a syntax error on Python >=3.7.0, pysnmp.asyncio does not work on 3.7 and above"")
+    print("WARNING :: Skipping tests due to asyncio.async() being a syntax error on Python >=3.7.0, pysnmp.asyncio does not work on 3.7 and above")
     sys.exit(0)
